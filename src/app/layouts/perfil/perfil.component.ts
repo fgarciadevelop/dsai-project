31a802f3dacc746f60376404c5185f16c17a5794
Fran García <fgarciadevelop@gmail.com>
2022-02-01 11:24:28 +0100

    Perfil fixed

--- src/app/layouts/perfil/perfil.component.ts
+++ src/app/layouts/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserModel } from 'src/app/api-connect/models/user.model';
 import { AuthService } from 'src/app/api-connect/services/auth.service';
@@ -10,14 +11,19 @@ import { AuthService } from 'src/app/api-connect/services/auth.service';
 })
 export class PerfilComponent implements OnInit {
 
+  public seePass: boolean = false;
   public userName: string = '';
   public userData!: UserModel;
   public permiso: string = '';
   public loading: boolean = false;
+  public repeatPassword: string = '';
+
+  public dataForm!: FormGroup;
 
   constructor(
     private auth: AuthService,
     private router: Router,
+    private fb: FormBuilder,
   ) { 
     this.loading = true;
     if(!this.auth.isAuthenticated()){
@@ -35,7 +41,7 @@ export class PerfilComponent implements OnInit {
           }else{
             this.permiso = 'Usuario';
           }
-          this.loading = false;
+          this.loadForm();
         })
       })
     }
@@ -46,6 +52,22 @@ export class PerfilComponent implements OnInit {
 
   public logout(){
     this.auth.logout();
+  }
+
+  loadForm(){
+
+    this.dataForm = this.fb.group({
+      userName: new FormControl(this.userData.userName, [Validators.required]),
+      email: new FormControl(this.userData.email, [Validators.required, Validators.email]),
+      password: new FormControl(this.userData.password, [Validators.required]),
+      repeatPassword: new FormControl(this.repeatPassword, [Validators.required])
+    });
+    this.loading = false;
+
+  }
+
+  public changeVisibility(){
+    this.seePass = !this.seePass;
   }
 
 }
